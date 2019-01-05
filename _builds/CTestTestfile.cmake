0c339900ef5caa86166035adbbbdebc49a48ee70
Roman <romanvas3008@gmail.com>
2019-01-05 00:08:39 +0000

    update main.cpp

--- _builds/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vagrant/Labs/lab-11-process
-# Build directory: /home/vagrant/Labs/lab-11-process/_builds
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/vagrant/Labs/lab-11-process/_builds/builder")
